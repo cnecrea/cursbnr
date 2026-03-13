301ed52dfbfdbbf4b4772b0f2497df99940e9377
cnecrea <48139709+cnecrea@users.noreply.github.com>
2026-03-14 00:17:11 +0100

    Update const.py

--- custom_components/cursbnr/const.py
+++ custom_components/cursbnr/const.py
@@ -2,7 +2,7 @@
 DOMAIN = "cursbnr"
 
 # URL pentru API-ul BNR
-URL = "https://dev.syspro.ro/homeassistant/cursbnr_data.json"
+URL = "http://130.61.61.84/homeassistant/cursbnr_data.json""
 
 # Interval implicit de actualizare (în secunde)
 DEFAULT_UPDATE_INTERVAL = 300  # 5 minute
